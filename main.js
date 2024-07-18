afcc0e549f90971af84d61a40ab9da2f641afae3
Benjamin <RichMan@MacBook-Pro-RichMan.local>
2024-07-18 17:49:06 +0500

    4 oy 3 dars

---
+++ main.js
@@ -0,0 +1,39 @@
+// let num = prompt(`Type number`)
+// let num2 = prompt(`Type number`)
+// let num3 = prompt(`Type number`)
+// let num4 = prompt(`Type number`)
+// let num5 = prompt(`Type number`)
+
+
+// let num6 = Math.max( Number(num) , Number(num2) , Number(num3) , Number(num4) , Number(num5) ,)
+
+// alert(num6)
+
+
+
+
+
+
+let num = (`Now we will choose random number for you  :)`)
+
+alert (`${num}`)
+
+let num1 = (`${Math.round(Math.random() * 100 )}`)
+
+alert(`${num1}`)
+
+
+alert (`Now we will split and increase to 2 your number`)
+
+const num2 = num1 * 2
+
+const num3 = num1 / 2
+
+const num4 = (num1 % 3)
+
+alert (`${num1}
+We increased your number to 2 and we got: ${num2}
+We split your number to 2 and we got: ${num3} 
+We residual your number to 3 and we got: ${num4}`)
+
+console.log(num);
